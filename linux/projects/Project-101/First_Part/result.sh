5b5625b12e910b6b257b7c0d4f82b1ea33a6fd54
murat <murat.yarali@outlook.com>
2022-02-13 17:01:06 +0300

    result.sh_updated

--- linux/projects/Project-101/First_Part/result.sh
+++ linux/projects/Project-101/First_Part/result.sh
@@ -2,4 +2,5 @@
 
 echo -e "Bu script 12.02.2022 tarihinde saat 18:00'da nightfighter grubu tarafindan yapilmiştir.\n"
 
-cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance" | awk -F, '{print $1,$4,$10}' | tee result.txt
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $16,$32,$48,$64}'
+
